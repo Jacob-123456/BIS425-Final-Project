83a99bed0ffab594fede2bca149fd221f9b0a746
Jacob-123456 <98856019+Jacob-123456@users.noreply.github.com>
2026-05-07 04:54:07 -0400

    Update database.py
    added to prepare for teacher.py

--- database.py
+++ database.py
@@ -4,10 +4,25 @@ conn = sqlite3.connect("school.db")
 cursor = conn.cursor()
 
 # Create tables (based on your ERD)
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usertable (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    account_type TEXT,
+    security_question_answer TEXT   
+)
+""")
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS students (
-    student_id TEXT PRIMARY KEY,
-    name TEXT
+    student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    user_id INTEGER,
+               
+    foreign key(user_id) references usertable(user_id)
 )
 """)
 
@@ -20,17 +35,20 @@ CREATE TABLE IF NOT EXISTS subjects (
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS assignments (
-    assignment_id TEXT PRIMARY KEY,
-    subject_id TEXT,
-    title TEXT
+    assignment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_id INTEGER,
+    title TEXT,
+    max_score REAL,
+    FOREIGN KEY(class_id) REFERENCES classes(class_id)
+        
 )
 """)
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS results (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    student_id TEXT,
-    assignment_id TEXT,
+    student_id INTEGER,
+    assignment_id INTEGER,
     real_score REAL,
     letter TEXT,
     max_score REAL,
@@ -41,19 +59,47 @@ CREATE TABLE IF NOT EXISTS results (
 )
 """)
 
+
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS usertable (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    student_id TEXT,
-    username TEXT UNIQUE,
-    password TEXT,
-    account_type TEXT,
-    security_question_answer TEXT,
-    FOREIGN KEY(student_id) REFERENCES students(student_id)
-        
-               
+CREATE TABLE IF NOT EXISTS teachers (
+    teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    user_id INTEGER,
+    foreign key(user_id) references usertable(user_id)        
 )
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS faculty (
+    faculty_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    user_id INTEGER,
+    foreign key(user_id) references usertable(user_id)        
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS classes (
+    class_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    teacher_id INTEGER,
+    subject_id TEXT,
+    FOREIGN KEY(teacher_id) REFERENCES teachers(teacher_id),
+    FOREIGN KEY(subject_id) REFERENCES subjects(subject_id)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS class_students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_id INTEGER,
+    student_id INTEGER,
+    FOREIGN KEY(class_id) REFERENCES classes(class_id),
+    FOREIGN KEY(student_id) REFERENCES students(student_id)
+)
+""")
+
+
 
 conn.commit()
 conn.close()
