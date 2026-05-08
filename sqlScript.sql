93225b0e376a79be4912c20e33a52fc65b2cdac0
Jacob-123456 <98856019+Jacob-123456@users.noreply.github.com>
2026-05-07 22:38:41 -0400

    bug fixes

--- sqlScript.sql
+++ sqlScript.sql
@@ -1,6 +1,22 @@
 CREATE DATABASE IF NOT EXISTS school;
 USE school;
 
+#i wouldn't exactly do this in a real db but for debugging this is pretty necessary.
+SET SQL_SAFE_UPDATES = 0;
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE results;
+TRUNCATE TABLE assignment_submissions;
+TRUNCATE TABLE class_students;
+TRUNCATE TABLE assignments;
+TRUNCATE TABLE classes;
+TRUNCATE TABLE students;
+TRUNCATE TABLE teachers;
+TRUNCATE TABLE faculty;
+TRUNCATE TABLE subjects;
+TRUNCATE TABLE usertable;
+SET SQL_SAFE_UPDATES = 1;
+SET FOREIGN_KEY_CHECKS = 1;
+
 CREATE TABLE IF NOT EXISTS usertable (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE,
@@ -17,7 +33,7 @@ CREATE TABLE IF NOT EXISTS students (
 );
 
 CREATE TABLE IF NOT EXISTS subjects (
-    subject_id VARCHAR(50) PRIMARY KEY,
+    subject_id VARCHAR(50) PRIMARY KEY, #this should be auto increment but i fear I already have code in python file that works around this and i do not want to find and change that.
     name VARCHAR(255)
 );
 
@@ -71,3 +87,81 @@ CREATE TABLE IF NOT EXISTS class_students (
     FOREIGN KEY(class_id) REFERENCES classes(class_id),
     FOREIGN KEY(student_id) REFERENCES students(student_id)
 );
+
+CREATE TABLE IF NOT EXISTS assignment_submissions (
+		id INTEGER AUTO_INCREMENT PRIMARY KEY,
+		assignment_id INTEGER,
+		student_id INTEGER,
+		submitted BOOLEAN DEFAULT FALSE,
+		FOREIGN KEY(assignment_id) REFERENCES assignments(assignment_id),
+		FOREIGN KEY(student_id) REFERENCES students(student_id)
+            );
+
+##----------------- sample  -------------
+#i know i should've just built some function or something to build sample data instead of defining it 1 by 1. but i'm tired.
+
+USE school;
+
+
+-- 1. STUDENT (jacob)
+
+SET @input = 'jacob';
+
+INSERT IGNORE INTO usertable (username, password, account_type, security_question_answer)
+VALUES (@input, @input, 'Student', @input);
+
+SELECT user_id INTO @uid
+FROM usertable
+WHERE username = @input;
+
+INSERT INTO students (name, user_id)
+VALUES (@input, @uid);
+
+
+
+-- 2. TEACHER (paul)
+
+SET @input = 'paul';
+
+INSERT IGNORE INTO usertable (username, password, account_type, security_question_answer)
+VALUES (@input, @input, 'Teacher', @input);
+
+SET @uid = LAST_INSERT_ID();
+
+INSERT INTO teachers (name, user_id)
+VALUES (@input, @uid);
+
+
+
+-- 3. FACULTY (Chris)
+
+SET @input = 'chris';
+
+INSERT INTO usertable (username, password, account_type, security_question_answer)
+VALUES (@input, @input, 'Faculty', @input);
+
+SET @uid = LAST_INSERT_ID();
+
+INSERT INTO faculty (name, user_id)
+VALUES (@input, @uid);
+
+
+
+-- 4. FACULTY (Obama)
+
+SET @input = 'obama';
+
+INSERT IGNORE INTO usertable (username, password, account_type, security_question_answer)
+VALUES (@input, @input, 'Faculty', @input);
+
+
+
+INSERT IGNORE INTO faculty (name, user_id)
+VALUES (@input, @uid);
+
+
+-- 5. SUBJECTS
+
+INSERT IGNORE INTO subjects (subject_id, name)
+VALUES (1, 'math');
+
